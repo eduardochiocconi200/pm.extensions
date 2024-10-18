23106b759d46dd190c3882065a825af9ab75deb0
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2024-10-18 15:39:06 -0700

    Added support for variant compliance and against what reference variant

--- com.servicenow.processmining.extensions/server/pm/src/main/java/com/servicenow/processmining/extensions/pm/report/data/DataSourceVariantFindingComplianceReferencePath.java
+++ com.servicenow.processmining.extensions/server/pm/src/main/java/com/servicenow/processmining/extensions/pm/report/data/DataSourceVariantFindingComplianceReferencePath.java
@@ -3,13 +3,34 @@ package com.servicenow.processmining.extensions.pm.report.data;
 public class DataSourceVariantFindingComplianceReferencePath
     extends DataSourceFinding
 {
+    private String variantId = null;
+    private String referencePathVariantId = null;
+    private boolean isCompliant = false;
     private int variationPathAverage = -1;
     private int referencePathAverage = -1;
     private int variationTotalCases = -1;
 
-    public DataSourceVariantFindingComplianceReferencePath()
+    public DataSourceVariantFindingComplianceReferencePath(final String variantId)
     {
         super();
+        this.variantId = variantId;
+        this.referencePathVariantId = null;
+        this.isCompliant = false;
+    }
+
+    public String getVariantId()
+    {
+        return this.variantId;
+    }
+
+    public void setVariantIsCompliant(boolean isCompliant)
+    {
+        this.isCompliant = isCompliant;
+    }
+
+    public boolean getVariantIsCompliant()
+    {
+        return this.isCompliant;
     }
 
     public void setVariationPathAverage(int avgDuration)
@@ -50,5 +71,20 @@ public class DataSourceVariantFindingComplianceReferencePath
     public int getVariationTotalPossibleImprovementTime()
     {
         return getDelta() * getVariationPathAverage();
+    }
+
+    public String toString()
+    {
+        return "Variation Avg: " + getVariationPathAverage() + ", Reference Path Avg: " + getReferencePathAverage() + ", Delta: " + getDelta() + ", Variation Total Cases: " + getVariationTotalCases() + ", Possible Improvement Time: " + getVariationTotalPossibleImprovementTime();
+    }
+
+    public void setReferencePathVariantId(String id)
+    {
+        this.referencePathVariantId = id;
+    }
+
+    public String getReferencePathVariantId()
+    {
+        return this.referencePathVariantId;
     }
 }
