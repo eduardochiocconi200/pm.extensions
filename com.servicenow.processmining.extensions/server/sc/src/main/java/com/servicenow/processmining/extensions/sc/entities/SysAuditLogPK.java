aec9ad40060633ed03fa58d85ee2fd13aa787a13
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2024-11-14 20:42:41 -0800

    Added functionality to extend the key used to retrieve sys audit log entries using table and fieldname attributes

--- com.servicenow.processmining.extensions/server/sc/src/main/java/com/servicenow/processmining/extensions/sc/entities/SysAuditLogPK.java
+++ com.servicenow.processmining.extensions/server/sc/src/main/java/com/servicenow/processmining/extensions/sc/entities/SysAuditLogPK.java
@@ -6,16 +6,52 @@ public class SysAuditLogPK
     extends PrimaryKey
 {
     private String tableName = null;
+    private String fieldName = null;
+    private int resultSetSize = DEFAULT_RESULT_SET_SIZE;
 
     public SysAuditLogPK(final String tableName)
     {
         super();
         this.tableName = tableName;
+        this.resultSetSize = DEFAULT_RESULT_SET_SIZE;
+    }
+
+    public SysAuditLogPK(final String tableName, final int resultSetSize)
+    {
+        super();
+        this.tableName = tableName;
+        this.resultSetSize = resultSetSize;
+    }
+
+    public SysAuditLogPK(final String tableName, final String fieldName)
+    {
+        super();
+        this.tableName = tableName;
+        this.fieldName = fieldName;
+        this.resultSetSize = DEFAULT_RESULT_SET_SIZE;
+    }
+
+    public SysAuditLogPK(final String tableName, final String fieldName, final int resultSetSize)
+    {
+        super();
+        this.tableName = tableName;
+        this.fieldName = fieldName;
+        this.resultSetSize = resultSetSize;
     }
 
     public String getTableName()
     {
         return this.tableName;
+    }
+
+    public String getFieldName()
+    {
+        return this.fieldName;
+    }
+
+    public int getResultSetSize()
+    {
+        return this.resultSetSize;
     }
 
     public boolean equals(Object other)
@@ -29,6 +65,10 @@ public class SysAuditLogPK
         }
         SysAuditLogPK castOther = (SysAuditLogPK) other;
 
+        if (this.fieldName != null && castOther.fieldName != null) {
+            return this.tableName.equals(castOther.tableName) && this.fieldName.equals(castOther.fieldName);
+        }
+
         return this.tableName.equals(castOther.tableName);
     }
 
@@ -37,11 +77,21 @@ public class SysAuditLogPK
         final int prime = 31;
         int hash = 17;
         hash = hash * prime + this.tableName.hashCode();
+        if (this.fieldName != null) {
+            hash = hash * prime + this.fieldName.hashCode();
+        }
+
         return hash;
     }
 
     public String toString()
     {
+        if (this.fieldName != null) {
+            return this.tableName + ":" + this.fieldName;
+        }
+
         return this.tableName;
     }
+
+    private static final int DEFAULT_RESULT_SET_SIZE = 10000;
 }
