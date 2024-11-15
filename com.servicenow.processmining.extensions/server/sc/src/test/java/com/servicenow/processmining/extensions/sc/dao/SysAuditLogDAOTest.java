666dd207f42cba2b9a1c09875ccd3c6cd147fc4e
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2024-11-14 20:43:16 -0800

    Added tests to validate retrieving syslog audit records using table and table+fieldname

--- com.servicenow.processmining.extensions/server/sc/src/test/java/com/servicenow/processmining/extensions/sc/dao/SysAuditLogDAOTest.java
+++ com.servicenow.processmining.extensions/server/sc/src/test/java/com/servicenow/processmining/extensions/sc/dao/SysAuditLogDAOTest.java
@@ -4,6 +4,8 @@ import com.servicenow.processmining.extensions.sc.entities.SysAuditLog;
 import com.servicenow.processmining.extensions.sc.entities.SysAuditLogPK;
 import com.servicenow.processmining.extensions.sn.core.ServiceNowInstance;
 
+import java.util.ArrayList;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -15,17 +17,44 @@ public class SysAuditLogDAOTest
     @Test
     public void execute()
     {
-        sysAuditLogTest();
+        sysAuditLog1Test();
+        sysAuditLog2Test();
+        sysAuditLog3Test();
     }
 
-    private void sysAuditLogTest()
+    private void sysAuditLog1Test()
     {
         ServiceNowInstance instance = new ServiceNowInstance(snInstance, snUser, snPassword);
         SysAuditLogDAOREST wfDAO = new SysAuditLogDAOREST(instance);
-        SysAuditLog sysAuditLog = wfDAO.findById(new SysAuditLogPK("incident"));
+        SysAuditLog sysAuditLog = wfDAO.findById(new SysAuditLogPK("incident", 100));
         Assert.assertNotNull(sysAuditLog);
         Assert.assertTrue(sysAuditLog.getLog().size() > 0);
         logger.debug("Retrieved: (" + sysAuditLog.getLog().size() + ") history entries.");
+        System.out.println("Retrieved: (" + sysAuditLog.getLog().size() + ") history entries.");
+    }
+
+    private void sysAuditLog2Test()
+    {
+        ServiceNowInstance instance = new ServiceNowInstance(snInstance, snUser, snPassword);
+        SysAuditLogDAOREST wfDAO = new SysAuditLogDAOREST(instance);
+        SysAuditLog sysAuditLog = wfDAO.findById(new SysAuditLogPK("incident", "state", 100));
+        Assert.assertNotNull(sysAuditLog);
+        Assert.assertTrue(sysAuditLog.getLog().size() > 0);
+        logger.debug("Retrieved: (" + sysAuditLog.getLog().size() + ") history entries.");
+        System.out.println("Retrieved: (" + sysAuditLog.getLog().size() + ") history entries.");
+    }
+
+    private void sysAuditLog3Test()
+    {
+        ServiceNowInstance instance = new ServiceNowInstance(snInstance, snUser, snPassword);
+        ArrayList<String> incidentIds = new ArrayList<String>();
+
+        SysAuditLogDAOREST wfDAO = new SysAuditLogDAOREST(instance);
+        SysAuditLog sysAuditLog = wfDAO.findByIds(new SysAuditLogPK("incident", "state"), incidentIds);
+        Assert.assertNotNull(sysAuditLog);
+        Assert.assertTrue(sysAuditLog.getLog().size() > 0);
+        logger.debug("Retrieved: (" + sysAuditLog.getLog().size() + ") history entries.");
+        System.out.println("Retrieved: (" + sysAuditLog.getLog().size() + ") history entries.");
     }
 
     private static final Logger logger = LoggerFactory.getLogger(SysAuditLogDAOTest.class);
