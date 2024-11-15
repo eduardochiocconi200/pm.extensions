fc0a2088d49c3abd79144e0e9970cf5c3bee42fc
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2024-11-14 20:39:14 -0800

    Moved test to new package

--- com.servicenow.processmining.extensions/server/pm/src/test/java/com/servicenow/processmining/extensions/model/ProcessMiningModelParser1Test.java
+++ com.servicenow.processmining.extensions/server/pm/src/test/java/com/servicenow/processmining/extensions/model/ProcessMiningModelParser1Test.java
@@ -1,13 +1,13 @@
-package com.servicenow.processmining.extensions.report;
+package com.servicenow.processmining.extensions.model;
+
+import com.servicenow.processmining.extensions.pm.model.ProcessMiningModelParser;
+import com.servicenow.processmining.extensions.pm.report.ProcessMiningModelFilterDataSource;
+import com.servicenow.processmining.extensions.sn.core.TestUtility;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.servicenow.processmining.extensions.pm.model.ProcessMiningModelParser;
-import com.servicenow.processmining.extensions.pm.report.ProcessMiningModelFilterDataSource;
-import com.servicenow.processmining.extensions.sn.core.TestUtility;
 
 public class ProcessMiningModelParser1Test
 {
@@ -45,6 +45,18 @@ public class ProcessMiningModelParser1Test
         String processMiningModelJSONString = new TestUtility().loadProcessMiningModel("/model/payload1-v.json");
         ProcessMiningModelParser parser = new ProcessMiningModelParser("abc");
         Assert.assertTrue(parser.parse(processMiningModelJSONString));
+        Assert.assertEquals(10, parser.getProcessMiningModel().getVariants().size());
+        System.out.println("Keys: (" + parser.getProcessMiningModel().getVariants().keySet() + ")");
+        Assert.assertEquals(160, parser.getProcessMiningModel().getVariants().get("f4dbf4607130d4eedcbb2b977d7f6f42").getCaseIds().size());
+        Assert.assertEquals(11, parser.getProcessMiningModel().getVariants().get("84e2704c49e004085c8a7e4e68e49999").getCaseIds().size());
+        Assert.assertEquals(3, parser.getProcessMiningModel().getVariants().get("9f1c43f8dbe6f94a319b0fca20163972").getCaseIds().size());
+        Assert.assertEquals(33, parser.getProcessMiningModel().getVariants().get("2ccfdf270114304997b5c4e2796df02e").getCaseIds().size());
+        Assert.assertEquals(64, parser.getProcessMiningModel().getVariants().get("09cd5db9bec8d6fa3897047c27db49cb").getCaseIds().size());
+        Assert.assertEquals(22, parser.getProcessMiningModel().getVariants().get("b8918322789ef61f599c8b99533f2440").getCaseIds().size());
+        Assert.assertEquals(68, parser.getProcessMiningModel().getVariants().get("74fffa0cd22d7aab39b384816a039ee5").getCaseIds().size());
+        Assert.assertEquals(5, parser.getProcessMiningModel().getVariants().get("cd513baef3750bc7f152fb63cf6f8b2e").getCaseIds().size());
+        Assert.assertEquals(5, parser.getProcessMiningModel().getVariants().get("6ef275c218b512da1caeb4cf23254a95").getCaseIds().size());
+        Assert.assertEquals(4, parser.getProcessMiningModel().getVariants().get("5a12f781eac518bc2939cde766d97074").getCaseIds().size());
 
         // Run Variation Analysis.
         ProcessMiningModelFilterDataSource ds = new ProcessMiningModelFilterDataSource(parser.getProcessMiningModel());
