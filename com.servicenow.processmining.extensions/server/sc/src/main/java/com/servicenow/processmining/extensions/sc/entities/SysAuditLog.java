3994e364e8de403edc66481bc02dc530006d425f
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2024-11-16 16:13:06 -0800

    Added capability to filter log based on a specific fieldname

--- com.servicenow.processmining.extensions/server/sc/src/main/java/com/servicenow/processmining/extensions/sc/entities/SysAuditLog.java
+++ com.servicenow.processmining.extensions/server/sc/src/main/java/com/servicenow/processmining/extensions/sc/entities/SysAuditLog.java
@@ -1,6 +1,7 @@
 package com.servicenow.processmining.extensions.sc.entities;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.servicenow.processmining.extensions.sn.entities.ServiceNowEntity;
 
@@ -8,6 +9,7 @@ public class SysAuditLog
     extends ServiceNowEntity
 {
     private ArrayList<SysAuditEntry> log = null;
+    private HashMap<String, ArrayList<SysAuditEntry>> filteredLogs = null;
 
     public SysAuditLog(SysAuditLogPK pk)
     {
@@ -18,5 +20,30 @@ public class SysAuditLog
     public ArrayList<SysAuditEntry> getLog()
     {
         return this.log;
+    }
+
+    public ArrayList<SysAuditEntry> getFilteredLog(final String fieldName)
+    {
+        if (getFilteredLogs().get(fieldName) == null) {
+            ArrayList<SysAuditEntry> filteredLog = new ArrayList<SysAuditEntry>();
+            for (SysAuditEntry entry : getLog()) {
+                if (entry.getFieldName().equals(fieldName)) {
+                    filteredLog.add(entry);
+                }
+            }
+
+            getFilteredLogs().put(fieldName, filteredLog);
+        }
+
+        return getFilteredLogs().get(fieldName);
+    }
+
+    private HashMap<String, ArrayList<SysAuditEntry>> getFilteredLogs()
+    {
+        if (filteredLogs == null) {
+            this.filteredLogs = new HashMap<String, ArrayList<SysAuditEntry>>();
+        }
+
+        return this.filteredLogs;
     }
 }
