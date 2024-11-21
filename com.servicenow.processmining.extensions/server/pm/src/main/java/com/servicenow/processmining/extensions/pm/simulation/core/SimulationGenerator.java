4b1f65d3c347d2909977f04d81e6a13e20f96a9f
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2024-11-21 10:44:24 +0530

    Formatting

--- com.servicenow.processmining.extensions/server/pm/src/main/java/com/servicenow/processmining/extensions/pm/simulation/core/SimulationGenerator.java
+++ com.servicenow.processmining.extensions/server/pm/src/main/java/com/servicenow/processmining/extensions/pm/simulation/core/SimulationGenerator.java
@@ -1,13 +1,10 @@
 package com.servicenow.processmining.extensions.pm.simulation.core;
 
-public abstract class SimulationGenerator {
+public abstract class SimulationGenerator
+{
     public abstract int getNumberOfSumulatorInstances();
-
     public abstract int getNumberOfStartedSumulatorInstances();
-
     public abstract double getStartIncrementInverval();
-
     public abstract boolean createSimulationInstances();
-
     public abstract boolean createSimulationInstances(final double nextDispatchTime);
 }
