dc9e85c187098ba8d2b83d402b451c7d468215a0
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2025-01-13 20:17:19 -0800

    Refactored to handle 60 second default timeout issue

--- com.servicenow.processmining.extensions/server/sc/src/main/java/com/servicenow/processmining/extensions/sc/dao/SysAuditLogDAOREST.java
+++ com.servicenow.processmining.extensions/server/sc/src/main/java/com/servicenow/processmining/extensions/sc/dao/SysAuditLogDAOREST.java
@@ -37,6 +37,7 @@ public class SysAuditLogDAOREST
 		int batchSize = getBatchSize(id);
 		int maxResultSet = getMaxResultSetSize(id);
 		boolean continueRetrieving = true;
+		boolean timeoutRetried = false;
 		SysAuditLog sysAuditLog = new SysAuditLog(id);
 		logger.debug("Retrieving Sys Audit Logs: ");
 
@@ -59,43 +60,60 @@ public class SysAuditLogDAOREST
 
 			if (response == null || response != null && response.equals("")) {
 				logger.error("The requested REST API operation (SysAuditLogDAOREST.findById) could not complete successfully on ServiceNow instance: (" + getInstance().getInstance() + ")!");
-				return null;
-			}
-
-			JSONObject sysAuditLogResponse = new JSONObject(response);
-			if (sysAuditLogResponse.has("result")) {
-				JSONArray sysAuditLogEntries = sysAuditLogResponse.getJSONArray("result");
-				int processedEntries = 0;
-				for (int i=0; i < sysAuditLogEntries.length(); i++, processedEntries++) {
-					JSONObject resultObject = (JSONObject) sysAuditLogEntries.get(i);
-					if (resultObject.has("sys_id")) {
-						String sysId = resultObject.getString("sys_id");
-						String documentkey = resultObject.getString("documentkey");
-						String tablename = resultObject.getString("tablename");
-						String fieldname = resultObject.getString("fieldname");
-						String oldvalue = resultObject.getString("oldvalue");
-						String newvalue = resultObject.getString("newvalue");
-						String createdOn = resultObject.getString("sys_created_on");
-						String createdBy = resultObject.getString("sys_created_by");
-						String reason = resultObject.has("reason") ? resultObject.getString("reason") : "";
-
-						SysAuditEntry entry = new SysAuditEntry(new SysAuditEntryPK(sysId));
-						entry.setDocumentKey(documentkey);
-						entry.setTableName(tablename);
-						entry.setFieldName(fieldname);
-						entry.setOldValue(oldvalue);
-						entry.setNewValue(newvalue);
-						entry.setSysCreatedOn(createdOn);
-						entry.setSysCreatedBy(createdBy);
-						entry.setReason(reason);
-
-						sysAuditLog.getLog().add(entry);
+				// If it is within the reasonable 60 second timeout error, we will give it one more shot. If not, we need to fail since it may be another problem.
+				// Unfortunately, we need to follow this logic since the Table REST API raises a generic 500 error without indicating this was a timeout issue.
+				long timeElapsed = endTime - startTime;
+				if (timeElapsed > 56000 && timeElapsed < 64000) {
+					logger.error("The requested REST API operation (SysAuditLogDAOREST.findById) took approx 1 min. So we will retry it one more time. Otherwise, we will show the message to extend the default 60 seconds timeout.");
+					// The default REST Table API timeout is 60 seconds. If after a retry (which probably forced a table caching and the second time the query can run faster),
+					// fails, then we fail the transaction and will need to show a message indicating that the default needs to be updated.
+					if (timeoutRetried) {
+						continueRetrieving = false;
+					}
+					else {
+						timeoutRetried = true;
 					}
 				}
+				else {
+					return null;
+				}
+			}
+			else {
+				JSONObject sysAuditLogResponse = new JSONObject(response);
+				if (sysAuditLogResponse.has("result")) {
+					JSONArray sysAuditLogEntries = sysAuditLogResponse.getJSONArray("result");
+					int processedEntries = 0;
+					for (int i=0; i < sysAuditLogEntries.length(); i++, processedEntries++) {
+						JSONObject resultObject = (JSONObject) sysAuditLogEntries.get(i);
+						if (resultObject.has("sys_id")) {
+							String sysId = resultObject.getString("sys_id");
+							String documentkey = resultObject.getString("documentkey");
+							String tablename = resultObject.getString("tablename");
+							String fieldname = resultObject.getString("fieldname");
+							String oldvalue = resultObject.getString("oldvalue");
+							String newvalue = resultObject.getString("newvalue");
+							String createdOn = resultObject.getString("sys_created_on");
+							String createdBy = resultObject.getString("sys_created_by");
+							String reason = resultObject.has("reason") ? resultObject.getString("reason") : "";
 
-				baseOffset += batchSize;
-				if (processedEntries < batchSize || baseOffset >= maxResultSet) {
-					continueRetrieving = false;
+							SysAuditEntry entry = new SysAuditEntry(new SysAuditEntryPK(sysId));
+							entry.setDocumentKey(documentkey);
+							entry.setTableName(tablename);
+							entry.setFieldName(fieldname);
+							entry.setOldValue(oldvalue);
+							entry.setNewValue(newvalue);
+							entry.setSysCreatedOn(createdOn);
+							entry.setSysCreatedBy(createdBy);
+							entry.setReason(reason);
+
+							sysAuditLog.getLog().add(entry);
+						}
+					}
+
+					baseOffset += batchSize;
+					if (processedEntries < batchSize || baseOffset >= maxResultSet) {
+						continueRetrieving = false;
+					}
 				}
 			}
 		} while (continueRetrieving);
