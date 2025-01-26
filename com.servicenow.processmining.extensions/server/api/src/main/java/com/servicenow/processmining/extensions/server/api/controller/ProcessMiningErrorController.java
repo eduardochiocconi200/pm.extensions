af427a61a09260cb2eae167ee1923b68461b282d
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2025-01-26 10:40:58 -0800

    Cosmetic

--- com.servicenow.processmining.extensions/server/api/src/main/java/com/servicenow/processmining/extensions/server/api/controller/ProcessMiningErrorController.java
+++ com.servicenow.processmining.extensions/server/api/src/main/java/com/servicenow/processmining/extensions/server/api/controller/ProcessMiningErrorController.java
@@ -16,7 +16,6 @@ public class ProcessMiningErrorController
 	public String handleError(HttpServletRequest request)
 	{
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-    
 		if (status != null) {
 			Integer statusCode = Integer.valueOf(status.toString());
 			if (statusCode == HttpStatus.NOT_FOUND.value()) {
