13f3d3d15cedbae435ea319728f06564b8930afa
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2025-01-26 10:46:20 -0800

    Added toString and JSON Payload methods

--- com.servicenow.processmining.extensions/server/pm/src/main/java/com/servicenow/processmining/extensions/pm/model/ProcessMiningModelFilterTransitions.java
+++ com.servicenow.processmining.extensions/server/pm/src/main/java/com/servicenow/processmining/extensions/pm/model/ProcessMiningModelFilterTransitions.java
@@ -1,8 +1,10 @@
 package com.servicenow.processmining.extensions.pm.model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 public class ProcessMiningModelFilterTransitions
+    implements Serializable
 {
     private ArrayList<ProcessMiningModelFilterTransitionsNode> transitionNodes = null;
 
@@ -17,16 +19,41 @@ public class ProcessMiningModelFilterTransitions
 
     public boolean addTransitionsNode(final ProcessMiningModelFilterTransitionsNode transitionNode)
     {
-        if (transitionNodes == null) {
-            transitionNodes = new ArrayList<ProcessMiningModelFilterTransitionsNode>();
-        }
-
-        return transitionNodes.add(transitionNode);
+        return getTransitionNodes().add(transitionNode);
     }
 
     public ArrayList<ProcessMiningModelFilterTransitionsNode> getTransitionNodes()
     {
+        if (transitionNodes == null) {
+            transitionNodes = new ArrayList<ProcessMiningModelFilterTransitionsNode>();
+        }
+
         return this.transitionNodes;
     }
-    
+
+    public String getFilterTransitionsJSON()
+    {
+        String json = " [ ";
+
+        boolean processedFirst = false;
+        for (ProcessMiningModelFilterTransitionsNode t : transitionNodes) {
+            if (processedFirst) {
+                json += ", ";
+            }
+            json += t.getTransitionNodeJSON();
+
+            processedFirst = true;
+        }
+
+        json += "]";
+
+        return json;
+    }
+
+    public String toString()
+    {
+        StringBuffer sb = new StringBuffer();
+        sb.append("[ Transition Nodes: (" + transitionNodes + ")]");
+        return sb.toString();
+    }
 }
