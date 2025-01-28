c22c65fb9c440c004e205078c3b2ca0f08b8c73c
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2025-01-28 15:08:53 -0800

    Updated copyright

--- com.servicenow.processmining.extensions/client/react-chrome-extension/sn-catalog-app/src/components/Footer.tsx
+++ com.servicenow.processmining.extensions/client/react-chrome-extension/sn-catalog-app/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 
 function Footer() {
-    return (<h6>ServiceNow Process Mining Extension - by Eduardo Chiocconi @ 2024</h6>);
+    return (<h6>ServiceNow Process Mining Extension - by Eduardo Chiocconi @ 2025</h6>);
 }
 
 export default Footer;
