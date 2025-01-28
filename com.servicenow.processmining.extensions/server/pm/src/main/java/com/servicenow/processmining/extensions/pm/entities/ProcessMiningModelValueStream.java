ff1010d5619264e989b21fbdc45904c0b1dcffc5
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2025-01-28 15:31:58 -0800

    Added ValueStream as a class attribute

--- com.servicenow.processmining.extensions/server/pm/src/main/java/com/servicenow/processmining/extensions/pm/entities/ProcessMiningModelValueStream.java
+++ com.servicenow.processmining.extensions/server/pm/src/main/java/com/servicenow/processmining/extensions/pm/entities/ProcessMiningModelValueStream.java
@@ -1,12 +1,25 @@
 package com.servicenow.processmining.extensions.pm.entities;
 
+import com.servicenow.processmining.extensions.pm.analysis.goldratt.ValueStream;
 import com.servicenow.processmining.extensions.sn.entities.ServiceNowEntity;
 
 public class ProcessMiningModelValueStream
     extends ServiceNowEntity
 {
+    private ValueStream valueStream = null;
+
     public ProcessMiningModelValueStream(ProcessMiningModelValueStreamPK pk)
     {
         super(pk);
+    }
+
+    public ValueStream getValueStream()
+    {
+        return this.valueStream;
+    }
+
+    public void setValueStream(final ValueStream vStream)
+    {
+        this.valueStream = vStream;
     }
 }
