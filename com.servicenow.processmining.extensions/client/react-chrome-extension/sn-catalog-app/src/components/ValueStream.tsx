aa51e1443b4f44b10fcca4b115f4f7fba1d02cbc
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2025-01-28 15:09:54 -0800

    Improved Value Stream UI

--- com.servicenow.processmining.extensions/client/react-chrome-extension/sn-catalog-app/src/components/ValueStream.tsx
+++ com.servicenow.processmining.extensions/client/react-chrome-extension/sn-catalog-app/src/components/ValueStream.tsx
@@ -1,81 +1,153 @@
-import { useState } from "react";
-import { CardGroup, Card, Container } from 'react-bootstrap'
+import { useEffect, useState } from "react";
+import { Button, CardGroup, Card, Col, Container, Row } from 'react-bootstrap';
+import axios from "axios";
 
-function ValueStream() {
-  const [selected, setSelected] = useState('phase3');
+interface ValueStreamType {
+  name: string,
+  nodes: string[],
+  statistics: {
+    summary: string,
+    measures: string[]
+  }
+}
+
+function ValueStream(props: { model: string; }) {
+  const [selectedPhase, setSelectedPhase] = useState<string>('');
+  const [phases, setPhases] = useState<ValueStreamType[]>([]);
+
+  function getValueStream() {
+    const url = 'http://localhost:8080/models/' + props.model + '/valuestream';
+    setPhases([]);
+    axios.get(url, { headers: { "Access-Control-Allow-Origin" : "*"} })
+      .then(response => response.data)
+      .then((data) => {
+        console.log('Retrieved Value Stream for model: ' + props.model);
+        const phases = data.valueStream.phases;
+        setPhases(phases);
+      })
+      .catch((err) => {
+        console.log("AXIOS ERROR: ", err)
+      })
+  }
+
+  const saveValueStream = (model: string) => {
+    const url = 'http://localhost:8080/models/' + model + '/valuestream';
+    axios.put(url, { responseType: "blob", headers: { "Access-Control-Allow-Origin" : "*"} })
+      .then(response => response.data)
+      .then((data) => {
+        console.log('Saving Value Stream: ' + data + '.');
+      })
+      .catch((err) => {
+        console.log("AXIOS ERROR: ", err)
+      })
+  };
+
+  const resetValueStream = (model: string) => {
+    const url = 'http://localhost:8080/models/' + model + '/valuestream';
+    axios.delete(url, { responseType: "blob", headers: { "Access-Control-Allow-Origin" : "*"} })
+      .then(response => response.data)
+      .then((data) => {
+        console.log('Resetting Value Stream: ' + data + '.');
+        setPhases([]);
+        setSelectedPhase('');
+      })
+      .catch((err) => {
+        console.log("AXIOS ERROR: ", err)
+      })
+  };
+
+  const runValueStreamAnalysis = (model: string) => {
+    const url = 'http://localhost:8080/models/' + model + '/valuestream/run';
+    axios.put(url, { responseType: "blob", headers: { "Access-Control-Allow-Origin" : "*"} })
+      .then(response => response.data)
+      .then((data) => {
+        console.log('Run Value Stream Analysis: ' + data + '.');
+      })
+      .catch((err) => {
+        console.log("AXIOS ERROR: ", err)
+      })
+  };
+
+  const addPhase = () => {
+    console.log("Adding Phase");
+    const newPhases = Array.from(phases);
+    newPhases.push({ "name" : "Phase " + (phases.length+1), "nodes" : [], statistics : { "summary" : "Summary", "measures" : [] } });
+    setPhases(newPhases);
+    console.log(phases.length);
+  };
 
   const showPhaseDetails = (phase: string) => {
-    alert('Current: ' + selected + ', New: ' + phase);
-    setSelected(phase);
-  }
-        
+    console.log('Current: ' + selectedPhase + ', New: ' + phase);
+    console.log(phases);
+    setSelectedPhase(phase);
+  };
+
+  useEffect(() => {
+    getValueStream();
+  }, []);
+
   return (
     <>
       <Container style={{ margin: '25px'}}>
         <CardGroup>
-          <Card border="light" onClick={ () => showPhaseDetails('phase1') } style={{ cursor: "pointer" }}>
-            <Card.Img variant="left" src="src/images/chevron.png"/>
-            <Card.ImgOverlay>
-            <Card.Body style={{ textAlign: 'center' }}>
-              <Card.Title style={{}}>Phase 1</Card.Title>
-              <Card.Text style={{ fontSize: 12 }}>Avg: 4 hrs 15 mins</Card.Text>
-            </Card.Body>
-            </Card.ImgOverlay>
-          </Card>
-
-          <Card border="light" onClick={ () => showPhaseDetails('phase2') } style={{ cursor: "pointer" }}>
-            <Card.Img variant="left" src="src/images/chevron.png"/>
-            <Card.ImgOverlay>
-            <Card.Body style={{ textAlign: 'center' }}>
-              <Card.Title>Phase 2</Card.Title>
-              <Card.Text style={{ fontSize: 12 }}>Avg: 4 hrs 15 mins</Card.Text>
-            </Card.Body>
-            </Card.ImgOverlay>
-          </Card>
-
-          <Card border="light" onClick={ () => showPhaseDetails('phase3') } style={{ cursor: "pointer" }}>
-            <Card.Img variant="left" src="src/images/chevron.png"/>
-            <Card.ImgOverlay>
-            <Card.Body style={{ textAlign: 'center' }}>
-              <Card.Title>Phase 3</Card.Title>
-              <Card.Text style={{ fontSize: 12 }}>Avg: 4 hrs 15 mins</Card.Text>
-            </Card.Body>
-            </Card.ImgOverlay>
-          </Card>
-
-          <Card border="light" onClick={ () => showPhaseDetails('phase4') } style={{ cursor: "pointer" }}>
-            <Card.Img variant="left" src="src/images/chevron.png"/>
-            <Card.ImgOverlay>
-            <Card.Body style={{ textAlign: 'center' }}>
-              <Card.Title>Phase 4</Card.Title>
-              <Card.Text style={{ fontSize: 12 }}>Avg: 4 hrs 15 mins</Card.Text>
-            </Card.Body>
-            </Card.ImgOverlay>
-          </Card>
-
-          <Card border="light" onClick={ () => showPhaseDetails('phase5') } style={{ cursor: "pointer" }}>
-            <Card.Img variant="left" src="src/images/chevron.png"/>
-            <Card.ImgOverlay>
-            <Card.Body style={{ textAlign: 'center' }}>
-              <Card.Title>Phase 5</Card.Title>
-              <Card.Text style={{ fontSize: 12 }}>Avg: 4 hrs 15 mins</Card.Text>
-            </Card.Body>
-            </Card.ImgOverlay>
-          </Card>
+          {phases.length > 0 && phases.map((value, index) => (
+          <Row>
+            <Col md="12">
+              <Card border="light" onClick={ () => showPhaseDetails('phase' + index) } style={{ cursor: "pointer" }}>
+                <Card.Img variant="left" src="src/images/chevron.png"/>
+                <Card.ImgOverlay>
+                <Card.Body style={{ textAlign: 'center' }}>
+                  <Card.Title style={{ fontSize: 18 }}>{(value as ValueStreamType).name}</Card.Title>
+                  <Card.Text style={{ fontSize: 12 }}>Avg: {(value as ValueStreamType).name}<br></br>Click for details</Card.Text>
+                </Card.Body>
+                </Card.ImgOverlay>
+              </Card>
+            </Col>
+          </Row>
+          ))}
+          <Row>
+            <Col md="12">
+              <Card border="light" onClick={ () => addPhase() } style={{ cursor: "pointer" }}>
+                <Card.Img variant="left" src="src/images/chevron.png"/>
+                <Card.ImgOverlay>
+                <Card.Body style={{ textAlign: 'center' }}>
+                  <Card.Title style={{ fontSize: 18 }}>Add Phase</Card.Title>
+                  <Card.Text style={{ fontSize: 12 }}>Click on the chevron to add new phase</Card.Text>
+                </Card.Body>
+                </Card.ImgOverlay>
+              </Card>
+            </Col>
+          </Row>
         </CardGroup>
-      </Container>
-
-      <Container style={{ margin: '25px'}}>
-        <Card border="light">
-          <Card.Body>
-            <Card.Text style={{fontSize: 12}}><b>Details:</b><br></br>
-            - Reword: SCORE.<br></br>
-            - Savings: SCORE.<br></br>
-            - ROI: SCORE.<br></br>
-            fsdfsdfsd sdfasf sdfsdf sfsfds sdfsdf
-            </Card.Text>
-          </Card.Body>
-        </Card>
+        {selectedPhase != "" &&  <Container style={{ margin: '25px'}}>
+          <Card border="light">
+            <Card.Body>
+              <Card.Text style={{fontSize: 14}}><b>{selectedPhase} Details:</b><br></br>
+                <Container>
+                  <Row>
+                  <Col sm="2">
+                    - Nodes:<br></br> <Button size="sm">Add Node</Button>
+                  </Col>
+                  <Col sm="10">
+                    - Reword: SCORE.<br></br>
+                    - Savings: SCORE.<br></br>
+                    - ROI: SCORE.<br></br>
+                    fsdfsdfsd sdfasf sdfsdf sfsfds sdfsdf
+                  </Col>
+                  </Row>
+                </Container>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Container>}
+        <div style={{marginTop: '20px'}}></div>
+        <div id="Actions Assets">
+          <Button onClick={ ()=> { saveValueStream(props.model)}} variant="primary">Save</Button>
+          {' '}
+          <Button onClick={ ()=> { resetValueStream(props.model)}} variant="primary">Clear</Button>
+          {' '}
+          <Button onClick={ ()=> { runValueStreamAnalysis(props.model)}} variant="primary">Run Analysis</Button>
+        </div>
       </Container>
     </>
   );
