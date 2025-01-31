1cb5bfd165fb47e37f95d16e123424087a63519c
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2025-01-30 20:42:16 -0800

    Removed TODO tags

--- com.servicenow.processmining.extensions/server/sn/src/main/java/com/servicenow/processmining/extensions/sn/entities/SerializablePrimaryKey.java
+++ com.servicenow.processmining.extensions/server/sn/src/main/java/com/servicenow/processmining/extensions/sn/entities/SerializablePrimaryKey.java
@@ -8,14 +8,14 @@ public class SerializablePrimaryKey
     }
 
     @Override
-    public boolean equals(Object other) {
-        // TODO Auto-generated method stub
+    public boolean equals(Object other)
+    {
         throw new UnsupportedOperationException("Unimplemented method 'equals'");
     }
 
     @Override
-    public int hashCode() {
-        // TODO Auto-generated method stub
+    public int hashCode()
+    {
         throw new UnsupportedOperationException("Unimplemented method 'hashCode'");
     }    
 }
