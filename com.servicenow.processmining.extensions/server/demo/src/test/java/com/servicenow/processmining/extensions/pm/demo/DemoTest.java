b0b9b7431af3b4c81c72392db28091e1d73064be
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2025-05-01 17:48:06 -0700

    New Test Case

--- com.servicenow.processmining.extensions/server/demo/src/test/java/com/servicenow/processmining/extensions/pm/demo/DemoTest.java
+++ com.servicenow.processmining.extensions/server/demo/src/test/java/com/servicenow/processmining/extensions/pm/demo/DemoTest.java
@@ -11,13 +11,23 @@ public class DemoTest
     @Test
     public void test()
     {
-        DemoModelParser parser = new DemoModelParser("src/test/resources/Incident Demo Data.xlsx");
+        // DemoModelParser parser = new DemoModelParser("src/test/resources/Incident Demo Data.xlsx");
+        // DemoModelParser parser = new DemoModelParser("/Users/eduardo.chiocconi/Downloads/Incident Demo Data_DP2.xlsx");
+        DemoModelParser parser = new DemoModelParser("/Users/eduardo.chiocconi/Downloads/Case Demo Data_EC1.xlsx", "ITSM-Demo-Data");
         Assert.assertTrue(parser.parse());
 
         ServiceNowInstance instance = new ServiceNowInstance(snInstance, snUser, snPassword);
         DemoModelCases cases = new DemoModelCases(parser.getModel(), instance);
         Assert.assertTrue(cases.create());
     }
+
+//     protected static final String snInstance = "pascalewashproc2.service-now.com";
+//     protected static final String snUser = "admin";
+//     protected static final String snPassword = "Norway@67";
+
+//    protected static final String snInstance = "processminingec1demo.service-now.com";
+//    protected static final String snUser = "admin";
+//    protected static final String snPassword = "323ElatiCtDanville!";
 
     protected static final String snInstance = ServiceNowTestCredentials.getInstanceName();
     protected static final String snUser = ServiceNowTestCredentials.getUserName();
