de11f7049ff0013fe5d7c79204e21eff3cf5f53e
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2025-06-13 15:22:21 -0700

    New constructor signature to have a reference to simulator; Tracking usage and queuing max to report via statistics

--- com.servicenow.processmining.extensions/server/pm/src/main/java/com/servicenow/processmining/extensions/pm/simulation/workflow/SimulationStatistics.java
+++ com.servicenow.processmining.extensions/server/pm/src/main/java/com/servicenow/processmining/extensions/pm/simulation/workflow/SimulationStatistics.java
@@ -1,13 +1,22 @@
 package com.servicenow.processmining.extensions.pm.simulation.workflow;
 
+import com.servicenow.processmining.extensions.pm.simulation.core.Simulator;
+
 public class SimulationStatistics
 {
+    private WorkflowSimulator simulator = null;
     private int numberOfCreatedInstances = 0;
     private double startTime = 0;
     private double endTime = 0;
 
-    public SimulationStatistics()
+    public SimulationStatistics(final Simulator simulator)
     {
+        this.simulator = (WorkflowSimulator) simulator;
+    }
+
+    public WorkflowSimulator getSimulator()
+    {
+        return this.simulator;
     }
 
     public void incrementCreatedInstances()
@@ -49,7 +58,18 @@ public class SimulationStatistics
     {
         StringBuffer sb = new StringBuffer();
         sb.append("Total Simulation Time: " + getTotalSimulationTime() + "\n");
-        sb.append("Total simulated workflow instances: " + getNumberOfCreatedInstances());
+        sb.append("Total simulated workflow instances: " + getNumberOfCreatedInstances() + "\n");
+        sb.append("Activity Simulation Stats:\n");
+        for (String activityId : getSimulator().getSimulationState().getProcessModel().getNodes().keySet()) {
+            String activityName = getSimulator().getSimulationState().getProcessModel().getNodes().get(activityId).getName();
+            int activityResourceCapacity = -1;
+            if (getSimulator().getSimulationState().getProcessModel().getNodes().get(activityId).getResources() != null) {
+                activityResourceCapacity = getSimulator().getSimulationState().getProcessModel().getNodes().get(activityId).getResources().getCapacity();
+                int activityMaxResourceUsedCapacity = getSimulator().getSimulationState().getMaxActivityStateCount().get(activityId);
+                int activityMaxQueue = getSimulator().getSimulationState().getMaxActivityStateQueue().get(activityId);
+                sb.append("Activity (" + activityId + "-" + activityName + "): Available Resource Capacity: (" + (activityResourceCapacity == -1 ? "Unlimited" : activityResourceCapacity)+ "), Max Used Resource Capacity: (" + activityMaxResourceUsedCapacity + "), Max Waiting Queue size: (" + activityMaxQueue + ")\n");
+            }
+        }
 
         return sb.toString();
     }
