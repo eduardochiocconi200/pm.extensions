cf526d4efb7b8199f375fab722c05b57bcd679f3
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2025-09-02 16:02:46 -0700

    Use a more generic error handler

--- com.servicenow.processmining.extensions/server/api/src/main/java/com/servicenow/processmining/extensions/server/api/controller/ProcessMiningErrorController.java
+++ com.servicenow.processmining.extensions/server/api/src/main/java/com/servicenow/processmining/extensions/server/api/controller/ProcessMiningErrorController.java
@@ -1,31 +1,21 @@
 package com.servicenow.processmining.extensions.server.api.controller;
 
-import org.springframework.boot.web.servlet.error.ErrorController;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.http.HttpServletRequest;
-
-@RestController
+@ControllerAdvice
 public class ProcessMiningErrorController
-	implements ErrorController
 {
-	@RequestMapping("/error")
-	public String handleError(HttpServletRequest request)
-	{
-		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-		if (status != null) {
-			Integer statusCode = Integer.valueOf(status.toString());
-			if (statusCode == HttpStatus.NOT_FOUND.value()) {
-				return "error-404";
-			}
-			else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-				return "error-500";
-			}
-		}
-
-		return "error";
-    }
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Object> handleAllExceptions(Exception ex) {
+		ex.printStackTrace();
+		Map<String, Object> body = new HashMap<>();
+		body.put("message", ex.getMessage());
+		return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
