8eb812422ceef49a800695486d11613128f3c00f
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2025-09-14 18:47:47 -0700

    Added new test cases.

--- com.servicenow.processmining.extensions/server/pm/src/test/java/com/servicenow/processmining/extensions/simulation/workflow/WorkflowSimulatorReplayTest.java
+++ com.servicenow.processmining.extensions/server/pm/src/test/java/com/servicenow/processmining/extensions/simulation/workflow/WorkflowSimulatorReplayTest.java
@@ -8,8 +8,6 @@ import com.servicenow.processmining.extensions.pm.simulation.workflow.WorkflowSi
 import com.servicenow.processmining.extensions.sc.entities.SysAuditEntry;
 import com.servicenow.processmining.extensions.sc.entities.SysAuditLog;
 import com.servicenow.processmining.extensions.sc.entities.SysAuditLogPK;
-import com.servicenow.processmining.extensions.sn.core.ServiceNowInstance;
-import com.servicenow.processmining.extensions.sn.core.ServiceNowTestCredentials;
 import com.servicenow.processmining.extensions.sn.core.TestUtility;
 
 import org.junit.Assert;
@@ -20,32 +18,27 @@ import org.slf4j.LoggerFactory;
 
 public class WorkflowSimulatorReplayTest
 {
-  private ServiceNowInstance instance = null;
   private AuditLogSerializer serializer = null;
 
   @Test
   public void test()
   {
     logger.info("Starting Simulation Replay Tests ...");
-    boolean runOne = false;
+    boolean runOne = true;
     if (runOne) {
-      test2();
+      test9();
     }
     else {
       test1();
       test2();
       test3();
       test4();
+      test5();
+      test6();
+      test7();
+      test8();
+      test9();
     }
-  }
-
-  private ServiceNowInstance getInstance()
-  {
-      if (instance == null) {
-          instance = new ServiceNowInstance(snInstance, snUser, snPassword);
-      }
-
-      return instance;
   }
 
   private void test1()
@@ -128,13 +121,114 @@ public class WorkflowSimulatorReplayTest
     logger.info("Completing Simulation Replay Test1 in (" + (testCaseEndTime - testCaseStartTime) + ") ...");
   }
 
+  private void test5()
+  {
+    logger.info("Starting Simulation Replay Test5 ...");
+    long testCaseStartTime = System.currentTimeMillis();
+    long startTime = System.currentTimeMillis();
+    WorkflowInstance.setDisplay(new PrintSimulationState());
+    WorkflowSimulationSamples sample13 = new WorkflowSimulationSamplesTest13("");
+    WorkflowSimulator sim = new WorkflowSimulator(sample13.getModel(), getSysAuditLog5(), "incident", "state");
+    Assert.assertTrue(sim.validateEmptyQueues());
+    sim.run();
+    Assert.assertTrue(sim.validateEmptyQueues());
+    Assert.assertEquals(1, sim.getStatistics().getNumberOfCreatedInstances());
+    Assert.assertEquals(1.211688E9, sim.getStatistics().getTotalSimulationTime(), 0.0);
+    long endTime = System.currentTimeMillis();
+    logger.info("Completing Test Case in (" + (endTime - startTime) + ") ...");
+
+    long testCaseEndTime = System.currentTimeMillis();
+    logger.info("Completing Simulation Replay Test1 in (" + (testCaseEndTime - testCaseStartTime) + ") ...");
+  }
+
+  private void test6()
+  {
+    logger.info("Starting Simulation Replay Test6 ...");
+    long testCaseStartTime = System.currentTimeMillis();
+    long startTime = System.currentTimeMillis();
+    WorkflowInstance.setDisplay(new PrintSimulationState());
+    WorkflowSimulationSamples sample13 = new WorkflowSimulationSamplesTest13("");
+    WorkflowSimulator sim = new WorkflowSimulator(sample13.getModel(), getSysAuditLog6(), "incident", "state");
+    Assert.assertTrue(sim.validateEmptyQueues());
+    sim.run();
+    Assert.assertTrue(sim.validateEmptyQueues());
+    Assert.assertEquals(1, sim.getStatistics().getNumberOfCreatedInstances());
+    Assert.assertEquals(3.2705E7, sim.getStatistics().getTotalSimulationTime(), 0.0);
+    long endTime = System.currentTimeMillis();
+    logger.info("Completing Test Case in (" + (endTime - startTime) + ") ...");
+
+    long testCaseEndTime = System.currentTimeMillis();
+    logger.info("Completing Simulation Replay Test1 in (" + (testCaseEndTime - testCaseStartTime) + ") ...");
+  }
+
+  private void test7()
+  {
+    logger.info("Starting Simulation Replay Test7 ...");
+    long testCaseStartTime = System.currentTimeMillis();
+    long startTime = System.currentTimeMillis();
+    WorkflowInstance.setDisplay(new PrintSimulationState());
+    WorkflowSimulationSamples sample13 = new WorkflowSimulationSamplesTest13("");
+    WorkflowSimulator sim = new WorkflowSimulator(sample13.getModel(), getSysAuditLog7(), "incident", "state");
+    Assert.assertTrue(sim.validateEmptyQueues());
+    sim.run();
+    Assert.assertTrue(sim.validateEmptyQueues());
+    Assert.assertEquals(3, sim.getStatistics().getNumberOfCreatedInstances());
+    Assert.assertEquals(3.3004E7, sim.getStatistics().getTotalSimulationTime(), 0.0);
+    long endTime = System.currentTimeMillis();
+    logger.info("Completing Test Case in (" + (endTime - startTime) + ") ...");
+
+    long testCaseEndTime = System.currentTimeMillis();
+    logger.info("Completing Simulation Replay Test1 in (" + (testCaseEndTime - testCaseStartTime) + ") ...");
+  }
+
+  private void test8()
+  {
+    logger.info("Starting Simulation Replay Test8 ...");
+    long testCaseStartTime = System.currentTimeMillis();
+    long startTime = System.currentTimeMillis();
+    WorkflowInstance.setDisplay(new PrintSimulationState());
+    WorkflowSimulationSamples sample14 = new WorkflowSimulationSamplesTest14("");
+    WorkflowSimulator sim = new WorkflowSimulator(sample14.getModel(), getSysAuditLog7(), "incident", "state");
+    Assert.assertTrue(sim.validateEmptyQueues());
+    sim.run();
+    Assert.assertTrue(sim.validateEmptyQueues());
+    Assert.assertEquals(3, sim.getStatistics().getNumberOfCreatedInstances());
+    Assert.assertEquals(1.05615E8, sim.getStatistics().getTotalSimulationTime(), 0.0);
+    long endTime = System.currentTimeMillis();
+    logger.info("Completing Test Case in (" + (endTime - startTime) + ") ...");
+
+    long testCaseEndTime = System.currentTimeMillis();
+    logger.info("Completing Simulation Replay Test1 in (" + (testCaseEndTime - testCaseStartTime) + ") ...");
+  }
+
+  private void test9()
+  {
+    logger.info("Starting Simulation Replay Test9 ...");
+    long testCaseStartTime = System.currentTimeMillis();
+    long startTime = System.currentTimeMillis();
+    WorkflowInstance.setDisplay(new PrintSimulationState());
+    WorkflowSimulationSamples sample14 = new WorkflowSimulationSamplesTest14("");
+    WorkflowSimulator sim = new WorkflowSimulator(sample14.getModel(), getSysAuditLog8(), "incident", "state");
+    Assert.assertTrue(sim.validateEmptyQueues());
+    sim.run();
+    Assert.assertTrue(sim.validateEmptyQueues());
+    Assert.assertEquals(1, sim.getStatistics().getNumberOfCreatedInstances());
+    Assert.assertEquals(9.2105E7, sim.getStatistics().getTotalSimulationTime(), 0.0);
+    long endTime = System.currentTimeMillis();
+    logger.info("Completing Test Case in (" + (endTime - startTime) + ") ...");
+
+    long testCaseEndTime = System.currentTimeMillis();
+    logger.info("Completing Simulation Replay Test1 in (" + (testCaseEndTime - testCaseStartTime) + ") ...");
+  }
+
   private void loadAuditData()
   {
     if (serializer == null) {
       serializer = new AuditLogSerializer();
-      String auditLogJSONString = new TestUtility().loadProcessMiningModel("/simulation/audit-log-1.json");
-      Assert.assertTrue(serializer.parse(auditLogJSONString));
     }
+
+    String auditLogJSONString = new TestUtility().loadProcessMiningModel("/simulation/audit-log-1.json");
+    Assert.assertTrue(serializer.parse(auditLogJSONString));
   }
 
   // Load one record: da3a07c7936886506e79bb1e1dba1025
@@ -206,9 +300,93 @@ public class WorkflowSimulatorReplayTest
     return stateAuditLog;
   }
 
-  private static final String snInstance = ServiceNowTestCredentials.getInstanceName();
-  private static final String snUser = ServiceNowTestCredentials.getUserName();
-  private static final String snPassword = ServiceNowTestCredentials.getPassword();
+  private void loadAuditData2()
+  {
+    if (serializer == null) {
+      serializer = new AuditLogSerializer();
+    }
+    String auditLogJSONString = new TestUtility().loadProcessMiningModel("/simulation/audit-log-2.json");
+    Assert.assertTrue(serializer.parse(auditLogJSONString));
+  }  
+
+  private SysAuditLog getSysAuditLog5()
+  {
+    loadAuditData2();
+    SysAuditLog stateAuditLog = new SysAuditLog((SysAuditLogPK) serializer.getLog().getPK());
+    for (SysAuditEntry se : serializer.getLog().getFieldNameFilteredLog("state")) {
+      stateAuditLog.getLog().add(se);
+    }
+
+    Assert.assertEquals(2, stateAuditLog.getLog().size());
+
+    return stateAuditLog;
+  }
+
+  private void loadAuditData3()
+  {
+    if (serializer == null) {
+      serializer = new AuditLogSerializer();
+    }
+    String auditLogJSONString = new TestUtility().loadProcessMiningModel("/simulation/audit-log-3.json");
+    Assert.assertTrue(serializer.parse(auditLogJSONString));
+  }
+
+  private SysAuditLog getSysAuditLog6()
+  {
+    loadAuditData3();
+    SysAuditLog stateAuditLog = new SysAuditLog((SysAuditLogPK) serializer.getLog().getPK());
+    for (SysAuditEntry se : serializer.getLog().getFieldNameFilteredLog("state")) {
+      stateAuditLog.getLog().add(se);
+    }
+
+    Assert.assertEquals(6, stateAuditLog.getLog().size());
+
+    return stateAuditLog;
+  }
+
+  private void loadAuditData4()
+  {
+    if (serializer == null) {
+      serializer = new AuditLogSerializer();
+    }
+    String auditLogJSONString = new TestUtility().loadProcessMiningModel("/simulation/audit-log-4.json");
+    Assert.assertTrue(serializer.parse(auditLogJSONString));
+  }
+
+  private SysAuditLog getSysAuditLog7()
+  {
+    loadAuditData4();
+    SysAuditLog stateAuditLog = new SysAuditLog((SysAuditLogPK) serializer.getLog().getPK());
+    for (SysAuditEntry se : serializer.getLog().getFieldNameFilteredLog("state")) {
+      stateAuditLog.getLog().add(se);
+    }
+
+    Assert.assertEquals(18, stateAuditLog.getLog().size());
+
+    return stateAuditLog;
+  }
+
+  private void loadAuditData5()
+  {
+    if (serializer == null) {
+      serializer = new AuditLogSerializer();
+    }
+    String auditLogJSONString = new TestUtility().loadProcessMiningModel("/simulation/audit-log-rework-1.json");
+    Assert.assertTrue(serializer.parse(auditLogJSONString));
+  }
+
+  private SysAuditLog getSysAuditLog8()
+  {
+    loadAuditData5();
+    SysAuditLog stateAuditLog = new SysAuditLog((SysAuditLogPK) serializer.getLog().getPK());
+    for (SysAuditEntry se : serializer.getLog().getFieldNameFilteredLog("state")) {
+      stateAuditLog.getLog().add(se);
+    }
+
+    Assert.assertEquals(8, stateAuditLog.getLog().size());
+
+    return stateAuditLog;
+  }
 
   private static final Logger logger = LoggerFactory.getLogger(WorkflowSimulatorReplayTest.class);
 }
