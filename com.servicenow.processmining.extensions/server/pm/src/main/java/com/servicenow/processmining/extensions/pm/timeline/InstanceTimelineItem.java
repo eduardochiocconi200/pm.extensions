13cf6e3320cc98aa0532399367e99714e3454819
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2025-11-14 18:44:10 -0800

    Added new attributes to allow creating records from timeline events

--- com.servicenow.processmining.extensions/server/pm/src/main/java/com/servicenow/processmining/extensions/pm/timeline/InstanceTimelineItem.java
+++ com.servicenow.processmining.extensions/server/pm/src/main/java/com/servicenow/processmining/extensions/pm/timeline/InstanceTimelineItem.java
@@ -1,13 +1,69 @@
 package com.servicenow.processmining.extensions.pm.timeline;
 
+import java.util.HashMap;
+
 import org.joda.time.DateTime;
 
 public class InstanceTimelineItem
     extends TimelineItem
 {
+    private String tableName = null;
+    private String tabName = null;
+    private String sysId = null;
+    private HashMap<String, String> updateValues = null;
+    
     public InstanceTimelineItem(final String instanceId, final String startId, final String endId, final DateTime previousTime, final DateTime eventTime)
     {
         super(instanceId, startId, endId, eventTime, previousTime, null);
+    }
+
+    public void setTableName(final String tableName)
+    {
+        this.tableName = tableName;
+    }
+
+    public String getTableName()
+    {
+        return this.tableName;
+    }
+
+    public void setTabName(final String tabName)
+    {
+        this.tabName = tabName;
+    }
+
+    public String getTabName()
+    {
+        return this.tabName;
+    }
+
+    public void setSysId(final String sysId)
+    {
+        this.sysId = sysId;
+    }
+
+    public String getSysId()
+    {
+        return this.sysId;
+    }
+
+    public void setUpdateValues(final HashMap<String, String> updateValues)
+    {
+        this.updateValues = updateValues;
+    }
+
+    public HashMap<String, String> getUpdateValues()
+    {
+        return this.updateValues;
+    }
+
+    public boolean isStartEvent()
+    {
+        if (getPreviousEventTime() == null || (getPreviousEventTime().getMillis() == getEventTime().getMillis())) {
+            return true;
+        }
+
+        return false;
     }
 
     public String toString()
