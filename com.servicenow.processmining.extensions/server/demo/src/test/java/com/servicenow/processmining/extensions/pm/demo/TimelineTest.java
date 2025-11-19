727b2b4cecb764ad155bc7b864fc881b132c7b21
Eduardo Chiocconi (Quickflows) <eduardo.chiocconi@quickflows.com>
2025-11-18 19:48:34 -0800

    cosmetic

--- com.servicenow.processmining.extensions/server/demo/src/test/java/com/servicenow/processmining/extensions/pm/demo/TimelineTest.java
+++ com.servicenow.processmining.extensions/server/demo/src/test/java/com/servicenow/processmining/extensions/pm/demo/TimelineTest.java
@@ -10,17 +10,18 @@ public class TimelineTest
 
     public static void main(String[] args)
     {
+        boolean createInstances = true;
+        boolean createTasks = true;
+
         long startTime = System.currentTimeMillis();
         // DemoModelParser parser = new DemoModelParser("src/test/resources/Incident Demo Data.xlsx", "TM-Demo-2");
+        // DemoModelParser parser = new DemoModelParser("/Users/eduardo.chiocconi/Downloads/PM TM Demo Data.xlsx", "TM-Demo-4");
         DemoModelParser parser = new DemoModelParser("/Users/eduardo.chiocconi/Downloads/Incident Demo Data.xlsx", "TM-Demo-3");
         Assert.assertTrue(parser.parse());
 
         DemoModelTimeline timeline = new DemoModelTimeline(parser.getModel());
         Assert.assertTrue(timeline.create());
         // Assert.assertTrue(timeline.printSorted());
-
-        boolean createInstances = false;
-        boolean createTasks = true;
 
         ServiceNowInstance instance = new ServiceNowInstance(snInstance, snUser, snPassword);
 
